0108dd5b2cd82e055cb5ebf17083825161dab1d4
Ankitcitrus <ankit.garg2205@gmail.com>
2020-06-24 16:00:26 +0530

    Create featureConfig215.js

---
+++ featureConfig215.js
@@ -0,0 +1,49 @@
+{
+    "Home": [
+        {
+            "image": '',
+            "isImageLocal": true,
+            "onPageURL": '',
+            "toPageURL": 'OnBoardingKhata',
+            "titleText": {
+                "en": 'Import Khata Records',
+                "hi": 'आयात खाता रिकॉर्ड'
+            },
+            "description": {
+                "en":
+                    'Were you finding it difficult to manage different Khata records in different places? Now you can Import Khata Records from other apps.',
+                "hi":
+                    'क्या आपको अलग-अलग जगहों पर अलग-अलग खता रिकॉर्ड को प्रबंधित करना मुश्किल हो रहा है? अब आप अन्य एप्लिकेशन से खाता रिकॉर्ड आयात कर सकते हैं।'
+            },
+            "priority": 1,
+            "buttonText": {
+                "en": 'GO TO KHATA',
+                "hi": 'KHATA पर जाएंं'
+            },
+            "id": 1
+        },
+        {
+            "image": '',
+            "isImageLocal": true,
+            "onPageURL": '',
+            "toPageURL": 'OnBoardingKhata',
+            "titleText": {
+                "en": 'Import SUPERCOIN Records',
+                "hi": 'आयात खाता रिकॉर्ड'
+            },
+            "description": {
+                "en":
+                    'Were you finding it difficult to manage different Khata records in different places? Now you can Import Khata Records from other apps.',
+                "hi":
+                    'क्या आपको अलग-अलग जगहों पर अलग-अलग खता रिकॉर्ड को प्रबंधित करना मुश्किल हो रहा है? अब आप अन्य एप्लिकेशन से खाता रिकॉर्ड आयात कर सकते हैं।'
+            },
+            "priority": 1,
+            "buttonText": {
+                "en": 'GO TO SUPERCOIN',
+                "hi": 'KHATA पर जाएंं'
+            },
+            "id": 2
+        },
+        
+    ]
+};
